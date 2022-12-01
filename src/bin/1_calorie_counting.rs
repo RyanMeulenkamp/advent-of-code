160f3132eae20a7055e951a993bea6271c3be3e2
Ryan Meulenkamp <Ryan.Meulenkamp@Nedap.com>
2022-12-01 14:13:08 +0100

    Improve inspired by other people's solutions 🙈

--- src/bin/1_calorie_counting.rs
+++ src/bin/1_calorie_counting.rs
@@ -1,21 +1,21 @@
 use advent_of_code::read_input;
+use itertools::Itertools;
 
 fn count_calories(input: Vec<Vec<usize>>, top_n: usize) -> usize {
-    let mut vec: Vec<usize> = input
+    input
         .into_iter()
         .map(|calories| calories.into_iter().sum())
-        .collect();
-    vec.sort();
-    vec.reverse();
-    vec[0..top_n].into_iter().sum()
+        .sorted_by(|a, b| usize::cmp(b, a))
+        .take(top_n)
+        .sum()
 }
 
 fn parse_lines(input: &str) -> Vec<Vec<usize>> {
     input
+        .trim()
         .split("\n\n")
         .map(|elf| {
             elf.lines()
-                .filter(|line| !line.is_empty())
                 .map(|line| line.trim().parse().unwrap())
                 .collect()
         })
@@ -23,33 +23,42 @@ fn parse_lines(input: &str) -> Vec<Vec<usize>> {
 }
 
 fn main() {
-    println!("Answer 1: {:?}", count_calories(parse_lines(&read_input!()), 1));
-    println!("Answer 2: {:?}", count_calories(parse_lines(&read_input!()), 3));
+    println!(
+        "Answer 1: {:?}",
+        count_calories(parse_lines(&read_input!()), 1)
+    );
+    println!(
+        "Answer 2: {:?}",
+        count_calories(parse_lines(&read_input!()), 3)
+    );
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{count_calories, parse_lines};
+    use indoc::indoc;
 
-    const TEST_SET: &str = "
-1000
-2000
-3000
+    const TEST_SET: &str = indoc! {
+        "
+        1000
+        2000
+        3000
 
-4000
+        4000
 
-5000
-6000
+        5000
+        6000
 
-7000
-8000
-9000
+        7000
+        8000
+        9000
 
-10000
-";
+        10000
+        "
+    };
 
     #[test]
-    fn test_calory_count() {
+    fn test_calorie_count() {
         assert_eq!(24000, count_calories(parse_lines(TEST_SET), 1));
         assert_eq!(45000, count_calories(parse_lines(TEST_SET), 3));
     }
